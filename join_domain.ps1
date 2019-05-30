027bb7bedf73b5a334411e0e7b473d41951255fc
Brandon DeVault <brandondevault@gmail.com>
2019-05-29 21:16:28 -0500

    Update join_domain.ps1

--- join_domain.ps1
+++ join_domain.ps1
@@ -1,11 +1,11 @@
 # Grab the MAC Address and establish a unique computer name based on MAC
 $mac = (Get-WmiObject win32_networkadapterconfiguration -filter 'ipenabled = "true"').MACAddress
 $newMac = $mac.Substring(9,8) -replace ':',"-"
-$newName = "Win10-$newMac"
+$newName = "Windows-$newMac"
 
 # Sets the domain name and prompts for credentials
-$domain = "perched.local"
-$username = "$domain\dcadmin"
+$domain = "domain.local"
+$username = "$domain\domainadmin"
 $password = Read-Host -Prompt "Ender password for $username" -AsSecureString
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 
