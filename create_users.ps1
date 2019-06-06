6fb17c62120a5cd8ef6b31251cc37168b89d6b31
Brandon DeVault <brandondevault@gmail.com>
2019-06-06 09:35:21 -0500

    script for creating ad users

---
+++ create_users.ps1
@@ -0,0 +1,39 @@
+﻿Import-Module ActiveDirectory
+
+#Enter a path to your import CSV file
+$ADUsers = Import-csv .\users.csv
+
+foreach ($User in $ADUsers)
+{
+
+       $Username    = $User.username
+       $Password    = $User.password
+       $Firstname   = $User.firstname
+       $Lastname    = $User.lastname
+       $Department  = $User.department
+
+       #Check if the user account already exists in AD
+       if (Get-ADUser -F {SamAccountName -eq $Username})
+       {
+               #If user does exist, output a warning message
+               Write-Warning "A user account $Username has already exist in Active Directory."
+       }
+       else
+       {
+              #If a user does not exist then create a new user account
+          
+        #Account will be created in the OU listed in the $OU variable in the CSV file; don’t forget to change the domain name in the"-UserPrincipalName" variable
+              New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@yourdomain.com" `
+            -Name "$Firstname $Lastname" `
+            -GivenName $Firstname `
+            -Surname $Lastname `
+            -Enabled $True `
+            -ChangePasswordAtLogon $False `
+            -DisplayName "$Lastname, $Firstname" `
+            -Department $Department `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
+       }
+}
